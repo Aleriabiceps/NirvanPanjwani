e26da4e4aeed66b9b3b3c5d8bed58f4f48cae2cf
Aleriabiceps <NirvanPanjwani>
2023-09-21 03:01:31 +0800

    updates

--- src/components/LastFmWidget.js
+++ src/components/LastFmWidget.js
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import { PlayCircleOutlined } from '@ant-design/icons';
+import YouTubeSearch from 'youtube-search-api';
 
 const LastFmWidget = ({ apiKey, userName }) => {
   const [latestTrack, setLatestTrack] = useState(null);
@@ -17,18 +19,26 @@ const LastFmWidget = ({ apiKey, userName }) => {
     fetchData();
   }, [apiKey, userName]);
 
+  const handlePlayClick = () => {
+    if (latestTrack) {
+      // Construct a YouTube search URL using the title and artist
+      const searchQuery = `${latestTrack.name} ${latestTrack.artist['#text']}`;
+      const youtubeUrl = `https://www.youtube.com/results?search_query=${encodeURIComponent(searchQuery)}`;
+      
+      // Open the YouTube search URL in a new tab
+      window.open(youtubeUrl, '_blank');
+    }
+  };
+
   return (
     <div style={{ 
-    
       maxWidth: '800px', 
-     
- 
-      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)', // Added a subtle shadow
+      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
     }}>
       <h2 style={{ 
-        fontSize: '22px', // Increased font size for the heading
-        marginBottom: '2px', // Added spacing below the heading
-        color: 'yellow', // Changed text color
+        fontSize: '22px', 
+        marginBottom: '2px', 
+        color: 'yellow',
       }}>Now Playing</h2>
       {latestTrack ? (
         <div style={{ display: 'flex', alignItems: 'center' }}>
@@ -37,19 +47,20 @@ const LastFmWidget = ({ apiKey, userName }) => {
             height: '60px', 
             objectFit: 'cover', 
             marginRight: '10px', 
-            borderRadius: '5px', // Rounded corners for the image
+            borderRadius: '5px',
           }} src={latestTrack.image[2]['#text']} alt="Album cover" />
-        <div style={{ flexGrow: 1 }}>
-  <div><strong style={{ color: 'red' }}>Title:</strong> <span style={{ color: 'white' }}>{latestTrack.name}</span></div>
-  <div><strong style={{ color: 'red' }}>Artist:</strong> <span style={{ color: 'white' }}>{latestTrack.artist['#text']}</span></div>
-</div>
-
+          <button onClick={handlePlayClick} style={{ background: 'none', border: 'none', cursor: 'pointer' }}>
+            <PlayCircleOutlined style={{ fontSize: '24px', color: 'yellow' }} />
+          </button>
+          <div style={{ flexGrow: 1 }}>
+            <div><strong style={{ color: 'red' }}>Title:</strong> <span style={{ color: 'white' }}>{latestTrack.name}</span></div>
+            <div><strong style={{ color: 'red' }}>Artist:</strong> <span style={{ color: 'white' }}>{latestTrack.artist['#text']}</span></div>
+          </div>
         </div>
       ) : (
         <p>Loading...</p>
       )}
     </div>
-    
   );
 };
 
