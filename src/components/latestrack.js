b0816c8a7fff9ed496dcfe20ab37627e23b72e07
Aleriabiceps <NirvanPanjwani>
2023-09-27 01:45:23 +0800

    updates

--- src/components/latestrack.js
+++ src/components/latestrack.js
@@ -1,68 +1,67 @@
 import React, { useEffect, useState } from 'react';
+import { PlayCircleOutlined, CustomerServiceOutlined, ContainerOutlined } from '@ant-design/icons';
 
-import { PlayCircleOutlined, CustomerServiceOutlined, FileTextOutlined, ContainerOutlined } from '@ant-design/icons';
 const Latestrack = ({ apiKey, userName }) => {
-  const [latestTrack, setLatestTrack] = useState(null);
-  const [topAlbum, setTopAlbum] = useState(null);
-  const [topTrack, setTopTrack] = useState(null);
-  const [topArtist, setTopArtist] = useState(null);
+  const [topAlbums, setTopAlbums] = useState([]);
+  const [topTracks, setTopTracks] = useState([]);
+  const [topArtists, setTopArtists] = useState([]);
 
   useEffect(() => {
-    const fetchData = async (method) => {
+    const fetchData = async (method, setStateFunc) => {
       try {
-        const response = await fetch(`https://ws.audioscrobbler.com/2.0/?method=${method}&user=${userName}&api_key=${apiKey}&format=json&limit=1`);
+        const response = await fetch(`https://ws.audioscrobbler.com/2.0/?method=${method}&user=${userName}&api_key=${apiKey}&format=json&limit=3`);
         const data = await response.json();
-        return data;
+        setStateFunc(data.topalbums?.album || data.toptracks?.track || data.topartists?.artist || []);
       } catch (error) {
         console.error(`Error fetching ${method}:`, error);
       }
     };
 
-    const fetchAllData = async () => {
-      const [latestTrackData, topAlbumData, topTrackData, topArtistData] = await Promise.all([
-        fetchData('user.getrecenttracks'),
-        fetchData('user.gettopalbums'),
-        fetchData('user.gettoptracks'),
-        fetchData('user.gettopartists')
-      ]);
+    fetchData('user.gettopalbums', setTopAlbums);
+    fetchData('user.gettoptracks', setTopTracks);
+    fetchData('user.gettopartists', setTopArtists);
 
-      setLatestTrack(latestTrackData?.recenttracks?.track[0]);
-      setTopAlbum(topAlbumData?.topalbums?.album[0]);
-      setTopTrack(topTrackData?.toptracks?.track[0]);
-      setTopArtist(topArtistData?.topartists?.artist[0]);
-    };
-
-    fetchAllData();
   }, [apiKey, userName]);
 
   return (
-
-    <ul style={{ listStyleType: 'none', paddingLeft: '0', margin: '0' }}>
+    <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr', gap: '15px' }}>
+      
+    <div>
+      <h4><ContainerOutlined style={{ marginRight: '8px', color: '#3498db', marginBottom: '-30px' }} />Top Albums</h4>
+      <ul style={{ paddingLeft: '20px', margin: '0' }}>
+        {topAlbums.map((album, index) => (
+          <li key={index} style={{ marginBottom: '2px' }}>
+            <span style={{ fontWeight: '600', fontFamily: 'Arial, sans-serif' }}>{album.name}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
   
-    {topAlbum && (
-      <li style={{ marginBottom: '12px' }}>
-        <ContainerOutlined style={{ marginRight: '8px', color: '#3498db' }} />
-        <span style={{ fontWeight: '600', fontFamily: 'Arial, sans-serif' }}>Top Album: {topAlbum.name}</span>
-      </li>
-    )}
-    
-    {topTrack && (
-      <li style={{ marginBottom: '12px' }}>
-        <PlayCircleOutlined style={{ marginRight: '8px', color: '#e74c3c' }} />
-        <span style={{ fontWeight: '600', fontFamily: 'Arial, sans-serif' }}>Top Track: {topTrack.name}</span>
-      </li>
-    )}
-    
-    {topArtist && (
-      <li style={{ marginBottom: '12px' }}>
-        <CustomerServiceOutlined style={{ marginRight: '8px', color: '#2ecc71' }} />
-        <span style={{ fontWeight: '600', fontFamily: 'Arial, sans-serif' }}>Top Artist: {topArtist.name}</span>
-      </li>
-    )}
-    
-  </ul>
-
-
+    <div>
+      <h4><PlayCircleOutlined style={{ marginRight: '8px', color: '#e74c3c' }} />Top Tracks</h4>
+      <ul style={{ paddingLeft: '20px', margin: '0' }}>
+        {topTracks.map((track, index) => (
+          <li key={index} style={{ marginBottom: '2px' }}>
+            <span style={{ fontWeight: '600', fontFamily: 'Arial, sans-serif' }}>{track.name}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  
+    <div>
+      <h4><CustomerServiceOutlined style={{ marginRight: '8px', color: '#2ecc71' }} />Top Artists</h4>
+      <ul style={{ paddingLeft: '20px', margin: '0' }}>
+        {topArtists.map((artist, index) => (
+          <li key={index} style={{ marginBottom: '2px' }}>
+            <span style={{ fontWeight: '600', fontFamily: 'Arial, sans-serif' }}>{artist.name}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  
+  </div>
+  
+  
   );
 };
 
